82b4c2326ace1e2f1c4391e87d5b9bc84fd2e060
Phung Duy Tan <23021439@vnu.edu.vn>
2025-03-14 18:44:17 +0700

    Add files via upload

---
+++ Lect4.0_Assignments/bai.1.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+class linklist{
+public:
+    Node* head = NULL;
+    void print() {
+        Node* tmp = head;
+        while (tmp != NULL) {
+            cout << tmp->data << " ";
+            tmp = tmp->next;
+        }
+        cout << endl;
+        return;
+    }
+
+    void append(int x) {
+        Node* newNode = new Node(x);
+        if (head == NULL) {
+            head = newNode;
+        }
+        else {
+            Node* add = head;
+            while (add->next != NULL) {
+                add = add->next;
+            }
+            add->next = newNode;
+        }
+    }
+    void search(int x) {
+        Node* se = head;
+        int vitri = 0;
+        while (se != NULL) {
+            if (se->data == x) {
+                cout << vitri << endl;
+                return;
+            }
+            se = se->next;
+            vitri++;
+        }
+        cout << "NO" << endl;
+    }
+    void reverse() {
+        Node* prev = NULL;
+        Node* current = head;
+        Node* next = NULL;
+
+        while (current != NULL) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    linklist list;
+    for (int i = 0; i < n; i++) {
+        string op;
+        cin >> op;
+
+        if (op == "append") {
+            int x;
+            cin >> x;
+            list.append(x);
+        }
+        else if (op == "search") {
+            int x;
+            cin >> x;
+            list.search(x);
+        }
+        else if (op == "reverse") {
+            list.reverse();
+            list.print();
+        }
+    }
+    return 0;
+}
+
---
+++ Lect4.0_Assignments/bai.2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    int dut;
+    Node* next;
+    Node(int x, int p) {
+        data = x;
+        dut = p;
+        next = NULL;
+    }
+};
+class linklist {
+public:
+    Node* front = NULL;
+    linklist() {
+    }
+    void print() {
+        Node* tmp = front;
+        while (tmp != NULL) {
+            cout << "(" << tmp->data << ", " << tmp->dut << ");";
+            if (tmp->next != NULL) { cout << " "; }
+            tmp = tmp->next;
+        }
+        cout << endl;
+        return;
+    }
+
+    void enqueue(int x, int p) {
+        Node* newNode = new Node(x, p);
+        if (front == NULL || (front->dut) < p) {
+            newNode->next = front;
+            front = newNode;
+        }
+        else {
+            Node* temp = front;
+            while (temp->next != NULL && temp->next->dut >= p) {
+                temp = temp->next;
+            }
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void dequeue() {
+        if (front == NULL) {
+            cout << "Hàng đợi rỗng!" << endl;
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+
+    linklist list;
+    for (int i = 0; i < n; i++) {
+        string op;
+        cin >> op;
+
+        if (op == "enqueue") {
+            int x, p;
+            cin >> x >> p;
+            list.enqueue(x, p);
+        }
+        else if (op == "dequeue") {
+            list.dequeue();
+        }
+    }list.print();
+    return 0;
+}
+
